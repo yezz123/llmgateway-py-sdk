094bfbdc82917f19be93e35870bb81aa5134d9cc
Yasser Tahiri <yasserth19@gmail.com>
2025-06-10 01:51:57 +0100

    ✨ Implement LLMGateway API client

---
+++ llmgateway/__init__.py
@@ -0,0 +1,15 @@
+"""LLMGateway Python SDK Client."""
+
+from .client import LLMGatewayClient
+from .models import ChatCompletionRequest, ChatCompletionResponse, Message, Model, ModelList
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "LLMGatewayClient",
+    "ChatCompletionRequest",
+    "ChatCompletionResponse",
+    "Model",
+    "ModelList",
+    "Message",
+]
